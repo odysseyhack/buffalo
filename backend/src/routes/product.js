85d65a711425cf3ea5b8ad4f7a5ce944ea2d2803
rick-meerwaldt <rick.meerwaldt@apg.nl>
2019-04-13 15:42:30 +0200

    Updatetd routes, merged with Godwin

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,40 +1,40 @@
 const resourceService = require('../services/resourceService');
 
 module.exports = [{
-  method: 'POST',
-  path: '/api/product/give',
-  config: { auth: false, cors: true },
-  async handler(req, h) {
-    const registerTransaction = await resourceService.registerProduct(req.payload.seed, req.payload.product);
-    const giveTransaction = await resourceService.giveProduct(req.payload.seed, registerTransaction.id);
-    return giveTransaction.id;
+    method: 'POST',
+    path: '/api/product/give',
+    config: { auth: false, cors: true },
+    async handler(req, h) {
+      const registerTransaction = await resourceService.registerProduct(req.payload.seed, req.payload.product);
+      const giveTransaction = await resourceService.giveProduct(req.payload.seed, registerTransaction.id);
+      return giveTransaction.id
+    }
   },
-},
-{
-  method: 'POST',
-  path: '/api/product/return',
-  config: { auth: false, cors: true },
-  async handler(req, h) {
-    const asset = await resourceService.giveProduct(req.payload.seed, req.payload.transactionId);
-    return asset.id;
+  {
+    method: 'POST',
+    path: '/api/product/return',
+    config: { auth: false, cors: true },
+    async handler(req, h) {
+      const giveTransaction = await resourceService.giveProduct(req.payload.seed, req.payload.transactionId);
+      return giveTransaction.id
+    }
   },
-},
-{
-  method: 'POST',
-  path: '/api/product/take',
-  config: { auth: false, cors: true },
-  async handler(req, h) {
-    const asset = await resourceService.takeProduct(req.payload.seed, req.payload.transactionId);
-    return asset.id;
+  {
+    method: 'POST',
+    path: '/api/product/take',
+    config: { auth: false, cors: true },
+    async handler(req, h) {
+      const takeTransaction = await resourceService.takeProduct(req.payload.seed, req.payload.transactionId);
+      return takeTransaction.id
+    }
   },
-},
-{
-  method: 'GET',
-  path: '/api/products',
-  config: { auth: false, cors: true },
-  async handler(req, h) {
-    const assets = await resourceService.getProductsFromCommons();
-    return assets;
+  {
+    method: 'GET',
+    path: '/api/products',
+    config: { auth: false, cors: true },
+    async handler(req, h) {
+      const assets = await resourceService.getProductsFromCommons();
+      return assets;
+    },
   },
-},
-];
+  ];
